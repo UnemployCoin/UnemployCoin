b1066bb1f5d5796ecf8ccb142818ec8019b81861
UnemployCoin <unemploycoin@gmail.com>
2025-06-04 17:17:08 -0500

    Initial upload of UnemployCoin smart contract source code
    - ERC20 token contract for unemployment support meme coin.
    - Includes Ownable and Pausable features.
    - Updated to include pause functionality and ownership controls.
    - Prepared for deployment on Polygon/Matic network.
    
    This version is not yet deployed. Future updates will be pushed here before redeployment.

---
+++ UnemployCoin.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/security/Pausable.sol";
+
+contract UnemployCoin is ERC20, Ownable, Pausable {
+    constructor() ERC20("UnemployCoin", "UNEMP") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override
+        whenNotPaused
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
